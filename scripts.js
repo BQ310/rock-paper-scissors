0b5522ba94b51c58e6836a8e421636caa8f7ba02
bmquach <bmquach@ucsc.edu>
2023-10-16 10:33:02 -0700

    completed

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 function getComputerChoice() {
-    value = Math.random();
+    let value = Math.random();
     if (value < 0.33) {
         return "rock";
     } else if (value > 0.33 && value < 0.66) {
@@ -9,15 +9,35 @@ function getComputerChoice() {
     };
 };
 
-function round(playerSelection, computerSelection) {
-    ps = playerSelection.toLowerCase();
-    rules = {"rock" : "paper", "paper":"scissors","scissors":"rock"};
+function playRound(playerSelection, computerSelection) {
+    let ps = playerSelection.toLowerCase();
+    let rules = {"rock" : "paper", "paper" : "scissors", "scissors" : "rock"};
     if (ps == computerSelection) {
-        return "Draw!" + ps + "and" + computerSelection + "cancel!";
+        return [0, "Draw! " + ps + " and " + computerSelection + " cancel!"];
     }
-    if (rules[ps] == computerSelection) {
-        return "You Win!" + ps + "beats" + computerSelection;
+    if (rules[computerSelection] == ps) {
+        return [1, "You Win! " + ps + " beats " + computerSelection];
     } else {
-        return "You Lose!" + computerSelection + "beats" + ps;
+        return [-1, "You Lose! " + computerSelection + " beats " + ps];
     }
 };
+
+function game() {
+    let player = 0;
+    for (let i = 0; i < 5; i++) {
+        let ps = prompt("Enter your move", "rock");
+        let cs = getComputerChoice();
+        let result = playRound(ps, cs);
+        player += result[0];
+        console.log(result[1]);
+    };
+    if (player < 0) {
+        console.log("You lose");
+    } else if (player > 0) {
+        console.log("You Win");
+    } else {
+        console.log("Its a draw");
+    }
+};
+
+game();
